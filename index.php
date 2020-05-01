ba84451a0fa3d9d96c25e2cd582f08e4e0f0ae12
AltZh <altynbek.zhabykov@gmail.com>
2020-05-01 17:16:57 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
-$apiToken = "912350444:AAEXnwyIEONMxMYRkS9QSN8hnnJF8qVL58o";
-
-$data = [
-    'chat_id' => '780805049',
-    'text' => $_GET['text'];
-];
-
-$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
-// Do what you want with result
+    exit;
 ?>
